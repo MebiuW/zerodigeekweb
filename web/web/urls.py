1c57fde8d7fa65ee5b03c9d639fff48a63d1c015
35889036 <7705424@qq.com>
2016-08-07 15:24:56 +0800

    hello world fixed

--- web/web/urls.py
+++ web/web/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from view import hello
+from web.view import hello
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
