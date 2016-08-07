7aea6572719d2377e76763ae97382465dfa7af89
72770 <727705424@qq.com>
2016-08-07 15:32:13 +0800

    Hello World -- templates

--- web/web/view.py
+++ web/web/view.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse('Hello World')
+    context = {}
+    context['title'] = 'Hello World'
+    return render(request,'hello.html',context)
