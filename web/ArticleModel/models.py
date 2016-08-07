40ee82c6aeb7caa7515d8a6360e1b285928feb7b
72770 <727705424@qq.com>
2016-08-07 15:53:35 +0800

    Hello World -- db

--- web/ArticleModel/models.py
+++ web/ArticleModel/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=32)
+    author = models.CharField(max_length=16)
+    views = models.IntegerField(default=0)
