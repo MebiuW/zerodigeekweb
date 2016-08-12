0cea1755dfa53369092e27e292fe7488205c6837
72770 <727705424@qq.com>
2016-08-12 16:12:38 +0800

    +Editor

--- web/OnlineEditor/admin.py
+++ web/OnlineEditor/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import OnlineEditorModel
+from web.OnlineEditor.models import OnlineEditorModel
 
 # Register your models here.
 admin.site.register(OnlineEditorModel)
