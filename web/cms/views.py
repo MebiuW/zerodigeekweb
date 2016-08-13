cbd02f070b1913f445a59dcfdae35490f8ee934c
72770 <727705424@qq.com>
2016-08-13 18:11:26 +0800

    + update

--- web/cms/views.py
+++ web/cms/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def show_list(request):
-    data = ArticlesModel.objects.all()[0]
+    data = ArticlesModel.objects.all().values()
     return HttpResponse(data)
