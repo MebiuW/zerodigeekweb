807630c198b692588cd6b77ea9858c57f12a0344
72770 <727705424@qq.com>
2016-08-13 17:44:34 +0800

    + update

--- web/ZeroEditor/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from ZeroEditor.models import ArticlesModel
-
-# Register your models here.
-admin.site.register(ArticlesModel)
--- web/ZeroEditor/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OnlineeditorConfig(AppConfig):
-    name = 'ZeroEditor'
