c64b6a4805116e6c852b57c5f20a9944b2ebfa54
72770 <727705424@qq.com>
2016-08-15 17:28:09 +0800

    + pre_Content

--- web/cms/models.py
+++ web/cms/models.py
@@ -7,6 +7,7 @@ class ArticlesModel(models.Model):
     title = models.CharField(max_length=64)
     source_type = models.IntegerField(default=1) #0 自动 1 原创 2转发 3 授权转发 4 未授权
     content = RichTextField(config_name='awesome_ckeditor')
+    pre_content = RichTextField(config_name='awesome_ckeditor', default="")
     author_name = models.CharField(default='佚名',max_length=16)
     create_time = models.DateTimeField()
     author_url = models.URLField()
